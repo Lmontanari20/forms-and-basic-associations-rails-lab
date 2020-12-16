1d09793b06c01ec95ed77126f11f06e205482faf
Lucas Montanari <Lmonty2011@gmail.com>
2020-12-16 10:37:10 -0800

    this lab took me way too long to figure out the last test...

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,8 @@
 class Genre < ActiveRecord::Base
   # add associations
+  has_many :songs
+
+  def self.genre_uniq
+    Genre.all.uniq
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,22 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :genre 
+  belongs_to :artist 
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_names=(names)
+    names.each do|name|
+      # note = Note.create(content: name)
+      self.notes.build(content: name)
+    end
+  end
+
 end
